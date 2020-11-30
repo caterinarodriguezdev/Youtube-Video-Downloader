171d1505ccd0efe902291cc5a299ddef48cebbc6
Caterina <crbergas@gmail.com>
2020-11-30 16:57:30 +0100

    feat: basic GUI

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ window.title('Torrapipes Youtube Video Downloader')
 label1 = Label(window, text="Paste Youtube Video Link Here", font=('bold', 20))
 label1.pack()
 
-linkField = Entry(window, width=60)
+linkVideo = StringVar()
+
+# textvariable attribute saves input text into the specified variable, in this case link
+linkField = Entry(window, width=60, textvariable='linkVideo')
 linkField.pack()
 
+def downloadVideo():
+    print("hey there biatch")
+    print(str(linkVideo.get()))
+    # youtubeVideo = YouTube(link).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    # youtubeVideo.download('C:/Users/Caterina/PycharmProjects/youtubeVideos')
+
+
 # command attribute shows which function will be ran when button is clicked
-downloadButton = Button(window, text='Download Video', width=20, bg='red', fg='white', command='downloadVideo')
+downloadButton = Button(window, text='Download Video', width=20, bg='red', fg='white', command=downloadVideo)
 downloadButton.pack()
+
 
 # This function will create an infinite loop that is used to run the application,
 # wait for an event to occur and process the event as long as the window is not closed.
@@ -22,5 +33,6 @@ window.mainloop()
 
 # "progressive=True" means that it will download the video but only for resolutions 720p and below.
 # Instead "adaptive=true" can be used.
-# youtubeVideo = YouTube('https://www.youtube.com/watch?v=tPEE9ZwTmy0').streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-# youtubeVideo.download('C:/Users/Caterina/PycharmProjects/youtubeVideos')
+
+
+
