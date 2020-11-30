783ab6c1754639137c46d5d0d480663b44d697c9
Caterina <crbergas@gmail.com>
2020-11-30 19:44:32 +0100

    feat: changed fonts and added a picture

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-
 from pytube import YouTube
 # I will use this module to create a Graphical User Interface
-from tkinter import StringVar
-from tkinter import Label
-from tkinter import Entry
-from tkinter import Tk
-from tkinter import messagebox
-from tkinter import Button
+from tkinter import StringVar, Label, Entry, Tk, messagebox, Button, Canvas, PhotoImage, font
+
 
 import os
 from pathlib import Path
 
 window = Tk()
-window.geometry('600x300')
+window.geometry('700x300')
 window.title('Torrapipes Youtube Video Downloader')
+window.configure(bg='#2A2F32')
 
-label1 = Label(window, text="Paste youtube video link here", font=('Montserrat', 20))
+label1 = Label(window, text="Paste a youtube video link here", font=('Adumu', 30), bg='#2A2F32', fg='white')
 label1.pack()
 
 linkVideo = StringVar()
@@ -24,8 +20,23 @@ linkVideo = StringVar()
 linkField = Entry(window, width=60, textvariable=linkVideo)
 linkField.pack()
 
+# Canvas for image
+canvas_width = 100
+canvas_height = 100
+
+canvas = Canvas(window,
+           width=canvas_width,
+           height=canvas_height, bd=0, highlightthickness=0, relief='ridge')
+canvas.configure(bg='red')
+canvas.pack()
+
+img = PhotoImage(file='assets/pyb.pgm')
+canvas.create_image(50,50, image=img)
+
 def downloadVideo():
     linkVideoUrl = linkVideo.get()
+    # "progressive=True" means that it will download the video but only for resolutions 720p and below.
+    # Instead "adaptive=true" can be used.
     youtubeVideo = YouTube(linkVideoUrl).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     if os.path.exists(os.path.join(Path.home(),'Descargas')):
         print("using windows spanish")
@@ -38,9 +49,10 @@ def downloadVideo():
     messagebox.showinfo('Video downloaded!', 'Find the video in your downloads folder :)')
 
 
-
+buttonFont = font.Font(family='Adumu', size=20)
 # command attribute shows which function will be ran when button is clicked
-downloadButton = Button(window, text='Download Video', width=20, bg='#26a8a8', fg='white', command=downloadVideo)
+downloadButton = Button(window, text='Download Video', width=20, bg='white', fg='#2A2F32', command=downloadVideo)
+downloadButton['font'] = buttonFont
 downloadButton.pack()
 
 
@@ -48,8 +60,7 @@ downloadButton.pack()
 # wait for an event to occur and process the event as long as the window is not closed.
 window.mainloop()
 
-# "progressive=True" means that it will download the video but only for resolutions 720p and below.
-# Instead "adaptive=true" can be used.
+
 
 
 
